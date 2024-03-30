7802ba49e273db19159cac4c73130cc011c53f79
Null <kelanidarasimi9@gmail.com>
2024-03-30 20:20:09 +0100

    added dynamici data and api route

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask,  render_template
+from flask import Flask,  render_template, jsonify
 
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        'id' : 1,
+        'title': "Data analyst",
+        'location': 'Benglaru India',
+        'salary': 'Rs. 10, 00, 000'
+    },
+    {
+        'id' : 2,
+        'title': "Data scientist",
+        'location': 'Delhi India',
+        'salary': 'Rs. 15, 00, 000'
+    },
+    {
+        'id' : 3,
+        'title': "Frontend engineering",
+        'location': 'Remote',
+        'salary': 'Rs. 12, 00, 000'
+    },
+    {
+        'id' : 4,
+        'title': "Backend engineering",
+        'location': 'Sanfrasisco USA',
+        'salary': '$ 12,000.00'
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+    
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
     
 
